15c13b83ed9ab0d411bbe4df6dc95db76c1a8932
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2023-06-07 16:09:14 -0600

    create Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rule;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,26 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+Route::get('/users', function (Request $request) {
+    return User::paginate(10);
+});
+Route::post('/users', function (Request $request) {
+    $validatedData = $request->validate([
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'email' => ['required', 'email', Rule::unique('users')],
+    ]);
+    User::create($validatedData);
+});
+Route::put('/users/{user}', function (Request $request, User $user) {
+    $validatedData = $request->validate([
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'email' => ['required', 'email', Rule::unique('users')->ignore($user->id)],
+    ]);
+    $user->update($validatedData);
+});
+Route::delete('/users/{user}', function (Request $request, User $user) {
+    $user->delete();
+});
