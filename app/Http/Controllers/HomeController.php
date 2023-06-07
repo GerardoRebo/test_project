c05a7ab75eaa2b48a6f944d34d8ae680b8ecbf24
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2023-06-07 16:57:09 -0600

    check the page query string value

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-
-        return view('home');
+        // $users = User::paginate(10);
+        $page = request()->query('page');
+        return view('home', compact('page'));
     }
 }
